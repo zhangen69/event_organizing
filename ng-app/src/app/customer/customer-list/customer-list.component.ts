c45f4e77359350f33a8dc1de77bf20eef7451765
Jacob <zhangen69@gmail.com>
2019-06-04 02:33:18 +0800

    update solution

--- ng-app/src/app/customer/customer-list/customer-list.component.ts
+++ ng-app/src/app/customer/customer-list/customer-list.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-list.component.css']
 })
 export class CustomerListComponent implements OnInit {
+  columns = [
+    { name: 'name', displayName: 'Name' },
+    { name: 'email', displayName: 'Email' },
+    { name: 'address', displayName: 'Address' },
+    { name: 'registrationNumber', displayName: 'Registration Number' },
+    { name: 'personInCharged.name', displayName: 'Person In Charged' },
+    { name: 'audit.updatedDate', displayName: 'Updated', type: 'date' },
+  ];
+  filterList = [
+    { type: 'name', display: 'Name', queryType: 'string' },
+    { type: 'email', display: 'Email', queryType: 'string' },
+    { type: 'address', display: 'Address', queryType: 'string' },
+    { type: 'registrationNumber', display: 'Registration Number', queryType: 'string' },
+  ];
 
   constructor() { }
 
