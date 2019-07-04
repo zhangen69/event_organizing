a3396c24c753481b2bcefcfee8a804ea6e5d97d0
Jacob Tan <zhangen69@live.co.uk>
2019-07-04 15:40:13 +0800

    Update attendee.routes.ts

--- node-app/src/routes/attendee.routes.ts
+++ node-app/src/routes/attendee.routes.ts
@@ -19,12 +19,18 @@ router.post(`/${service}/sendEmail`, (req, res, next) => {
         secure: true,
         auth,
     });
+    const mailTemplate = `
+        <h1>Congradulations!</h1>
+        <p>Dear ${req.body.name},</p>
+        <p>We're appriaciate you registered the event, here are your pass code</p>
+        <h3>${req.body.code}</h3>
+        <p>We are looking foward your attend, thank you.</p>
+    `;
     const mailOptions = {
-        from: `EvtOrgMs <${auth.user}>`, // sender address
-        to: 'zhangen69@gmail.com', // req.body.to, // list of receivers
-        subject: 'test mail', // req.body.subject, // Subject line
-        text: 'test mail body', // req.body.body, // plain text body
-        html: '<b>NodeJS Email Tutorial</b>', // html body
+        from: `EvtOrgMs <${auth.user}>`,
+        to: req.body.email,
+        subject: `Event Register Confirmation: ${req.body.eventName}`,
+        html: mailTemplate,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
@@ -33,7 +39,7 @@ router.post(`/${service}/sendEmail`, (req, res, next) => {
         }
         console.log('Message %s sent: %s', info.messageId, info.response);
         // res.render('index');
-        res.status(200).json({ message: 'successfully sent' });
+        res.status(200).json({ message: 'successfully sent', suceeded: true });
     });
 });
 
