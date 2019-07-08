c1ae5c770f325814dc1961a2b54c6fefb15be333
Jacob Tan <zhangen69@live.co.uk>
2019-07-08 18:39:23 +0800

    fixed email contains `undefined` in the subkect

--- node-app/src/routes/attendee.routes.ts
+++ node-app/src/routes/attendee.routes.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import express from 'express';
 import nodeMailer from 'nodemailer';
-import { from, Observable } from 'rxjs';
+import { from, Observable, Subscription } from 'rxjs';
 import { exhaust, exhaustMap, startWith } from 'rxjs/operators';
 import appConfigs from '../configs/app.configs';
 import Controller from '../standards/controller';
@@ -46,27 +46,36 @@ router.post(`/${service}/register`, (req, res, next) => {
         if (attendee.email) {
             console.log('sending email...');
 
-            const transporter = nodeMailer.createTransport(appConfigs.mail);
-            const mailTemplate = `
-                <h1>Congradulations!</h1>
-                <p>Dear ${attendee.name},</p>
-                <p>We're appriaciate you registered the event, here are your pass code</p>
-                <h3>${attendee.code}</h3>
-                <p>We are looking foward your attend, thank you.</p>
-            `;
-            const mailOptions = {
-                from: `EvtOrgMs <${appConfigs.mailAuth.user}>`,
-                to: attendee.email,
-                subject: `Event Register Confirmation: ${attendee.event.name}`,
-                html: mailTemplate,
+            const sendEmail = (event) => {
+                const transporter = nodeMailer.createTransport(appConfigs.mail);
+                const mailTemplate = `
+                    <h1>Congradulations!</h1>
+                    <p>Dear ${attendee.name},</p>
+                    <p>We're appriaciate you registered the event, here are your pass code</p>
+                    <h3>${attendee.code}</h3>
+                    <p>We are looking foward your attend, thank you.</p>
+                `;
+
+                const mailOptions = {
+                    from: `EvtOrgMs <${appConfigs.mailAuth.user}>`,
+                    to: attendee.email,
+                    subject: `Event Register Confirmation: ${event.name}`,
+                    html: mailTemplate,
+                };
+
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    console.log('Message %s sent: %s', info.messageId, info.response);
+                    res.status(200).json({ succeeded: true, message: 'Email sent.', data: attendee });
+                });
             };
 
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    return console.log(error);
-                }
-                console.log('Message %s sent: %s', info.messageId, info.response);
-                res.status(200).json({ succeeded: true, message: 'Email sent.', data: attendee });
+            const getEventInfo = from(axios.get('http://localhost:3000/service/event/' + attendee.event));
+            getEventInfo.subscribe((res: any) => {
+                console.log('event is gotchat!');
+                sendEmail(res.data);
             });
         }
     });
--- node-app/src/routes/event.routes.ts
+++ node-app/src/routes/event.routes.ts
@@ -4,6 +4,7 @@ import StandardRoutes from '../standards/routes';
 
 const service = 'event';
 const routes = new StandardRoutes(service, new Controller(service));
+routes.bypass.getItemById = true;
 const router = routes.router(express.Router());
 
 export default router;
