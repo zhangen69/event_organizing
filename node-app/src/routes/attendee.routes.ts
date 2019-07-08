5e7a6e8d30eda1efb2bbd488bf95a319bee941dc
Jacob Tan <zhangen69@live.co.uk>
2019-07-08 23:02:16 +0800

    send qrcode image in email

--- node-app/src/routes/attendee.routes.ts
+++ node-app/src/routes/attendee.routes.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import express from 'express';
+import fs from 'fs';
 import nodeMailer from 'nodemailer';
 import QRCode from 'qrcode';
-import { forkJoin, from, Observable, Subscription } from 'rxjs';
-import { exhaust, mergeMap, startWith, switchMap } from 'rxjs/operators';
+import { forkJoin, from } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 import appConfigs from '../configs/app.configs';
 import Controller from '../standards/controller';
 import StandardRoutes from '../standards/routes';
@@ -14,41 +15,7 @@ const routes = new StandardRoutes(service, controller);
 routes.bypass.create = true;
 const router = routes.router(express.Router());
 
-// router.post(`/${service}/sendEmail`, (req, res, next) => {
-//     console.log('sending email...');
-//     const transporter = nodeMailer.createTransport(appConfigs.mail);
-//     const mailTemplate = `
-//         <h1>Congradulations!</h1>
-//         <p>Dear ${req.body.name},</p>
-//         <p>We're appriaciate you registered the event, here are your pass code</p>
-//         <h3>${req.body.code}</h3>
-//         <p>We are looking foward your attend, thank you.</p>
-//     `;
-//     const mailOptions = {
-//         from: `EvtOrgMs <${appConfigs.mailAuth.user}>`,
-//         to: req.body.email,
-//         subject: `Event Register Confirmation: ${req.body.eventName}`,
-//         html: mailTemplate,
-//     };
-
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             return console.log(error);
-//         }
-//         console.log('Message %s sent: %s', info.messageId, info.response);
-//         // res.render('index');
-//         res.status(200).json({ message: 'successfully sent', suceeded: true });
-//     });
-// });
-
 router.post(`/${service}/register`, (req, res, next) => {
-    // axios.post('http://localhost:3000/service/attendee', req.body).then((response) => {
-    //     const attendee = response.data.data;
-    //     if (attendee.email) {
-
-    //     }
-    // });
-
     let attendee;
 
     const sendEmail = ([event, qrcode]) => {
@@ -60,21 +27,31 @@ router.post(`/${service}/register`, (req, res, next) => {
             <p>Dear ${attendee.name},</p>
             <p>We're appriaciate you registered the event, here are your pass code</p>
             <h3>${attendee.code}</h3>
-            <img src="${qrcode}" />
             <p>We are looking foward your attend, thank you.</p>
         `;
+
+        const tempFilename = 'out.png';
+        const base64Data = qrcode.replace(/^data:image\/png;base64,/, '');
+        fs.writeFileSync(tempFilename, base64Data, 'base64');
 
         const mailOptions = {
             from: `EvtOrgMs <${appConfigs.mailAuth.user}>`,
             to: attendee.email,
             subject: `Event Register Confirmation: ${event.name}`,
             html: mailTemplate,
+            attachments: [
+                {
+                    filename: `${event.name}-qrcode.png`,
+                    content: fs.createReadStream(tempFilename),
+                },
+            ],
         };
 
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
                 return console.log(error);
             }
+            fs.unlinkSync(tempFilename);
             console.log('Message %s sent: %s', info.messageId, info.response);
             res.status(200).json({ succeeded: true, message: 'Email sent.', data: attendee });
         });
@@ -84,19 +61,26 @@ router.post(`/${service}/register`, (req, res, next) => {
 
     // createAttendee > qrcode > getEventInfo
     const process = createAttendee.pipe(
-        mergeMap((res) => {
-            attendee = res.data;
-            const qrcode = from(QRCode.toDataURL(attendee.code));
-            const getEventInfo = from(axios.get('http://localhost:3000/service/event/' + attendee.event));
-            return forkJoin([getEventInfo, qrcode]);
+        map((res) => (attendee = res.data.data)),
+        mergeMap(() => {
+            const getEvent = from(axios.get('http://localhost:3000/service/event/' + attendee.event)).pipe(map((res) => res.data.data));
+            const generateQRCode = from(QRCode.toDataURL(attendee.code, { errorCorrectionLevel: 'H', type: 'image/png' }));
+            return forkJoin([getEvent, generateQRCode]);
         }),
     );
 
-    process.subscribe((results) => {
-        console.log('qrcode and event is gotchat!');
-        console.log(results);
-        sendEmail(results);
-    });
+    process.subscribe(
+        (results: any) => {
+            console.log('qrcode and event is gotcha!');
+            if (attendee.email) {
+                sendEmail(results);
+            }
+        },
+        (err) => {
+            console.log('error');
+            console.log(err);
+        },
+    );
 });
 
 export default router;
