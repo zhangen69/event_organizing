875522f365552a0fdc35c42b90b867f0e09fced7
Jacob <zhangen69@gmail.com>
2019-09-21 21:48:23 +0800

    import excel in angular (show data as json)

--- ng-app/src/app/demo-xlsx/demo-xlsx.component.ts
+++ ng-app/src/app/demo-xlsx/demo-xlsx.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { saveAs } from 'file-saver';
-import '@grapecity/spread-sheets-charts';
-import * as GC from '@grapecity/spread-sheets';
-import * as Excel from '@grapecity/spread-excelio';
+import * as XLSX from 'xlsx';
+import { from } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+
+type AOA = any[][];
 
 @Component({
   selector: 'app-demo-xlsx',
@@ -10,70 +11,56 @@ import * as Excel from '@grapecity/spread-excelio';
   styleUrls: ['./demo-xlsx.component.css']
 })
 export class DemoXlsxComponent implements OnInit {
-  spreadBackColor = 'aliceblue';
-  hostStyle = {
-    width: '95vw',
-    height: '80vh'
-  };
   excelData: any;
-  private spread: GC.Spread.Sheets.Workbook;
-  private excelIO;
+  jsonData = [];
+  // ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.excelData);
 
-  constructor() {
-    this.excelIO = new Excel.IO();
-  }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  workbookInit(args) {
-    const self = this;
-    self.spread = args.spread;
-    const sheet = self.spread.getActiveSheet();
-    // sheet.getCell(0, 0).text('Test Excel').foreColor('blue');
-    // sheet.getCell(1, 0).text('Test Excel').foreColor('blue');
-    // sheet.getCell(2, 0).text('Test Excel').foreColor('blue');
-    // sheet.getCell(3, 0).text('Test Excel').foreColor('blue');
-    // sheet.getCell(0, 1).text('Test Excel').foreColor('blue');
-    // sheet.getCell(1, 1).text('Test Excel').foreColor('blue');
-    // sheet.getCell(2, 1).text('Test Excel').foreColor('blue');
-    // sheet.getCell(3, 1).text('Test Excel').foreColor('blue');
-    // sheet.getCell(0, 2).text('Test Excel').foreColor('blue');
-    // sheet.getCell(1, 2).text('Test Excel').foreColor('blue');
-    // sheet.getCell(2, 2).text('Test Excel').foreColor('blue');
-    // sheet.getCell(3, 2).text('Test Excel').foreColor('blue');
-    // sheet.getCell(0, 3).text('Test Excel').foreColor('blue');
-    // sheet.getCell(1, 3).text('Test Excel').foreColor('blue');
-    // sheet.getCell(2, 3).text('Test Excel').foreColor('blue');
-    // sheet.getCell(3, 3).text('Test Excel').foreColor('blue');
-  }
-
-  onFileChange(args) {
-    const self = this;
-    const file = args.srcElement && args.srcElement.files && args.srcElement.files[0];
-
-    if (self.spread && file) {
-      self.excelIO.open(file, (json) => {
-        self.spread.fromJSON(json, {});
-        self.excelData = json.sheets.Sheet1.data.dataTable;
-        setTimeout(() => {
-          alert('load successfully');
-        }, 0);
-      }, (error) => {
-        alert('load fail');
-      });
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) {
+      throw new Error('Cannot use multiple files');
     }
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.excelData = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      this.onLoadExcelData();
+    };
+    reader.readAsBinaryString(target.files[0]);
   }
 
-  onClickMe(args) {
-    const self = this;
-    const filename = 'exportExcel.xlsx';
-    const json = JSON.stringify(self.spread.toJSON());
-    self.excelIO.save(json, function (blob) {
-      saveAs(blob, filename);
-    }, function (e) {
-      console.log(e);
-    });
+  onLoadExcelData() {
+    if (this.excelData) {
+      const headers = this.excelData[0].map((ele) => {
+        return {
+          displayName: ele,
+          name: ele.replace(/(^[A-Z])/, (str) => str.toLowerCase()).replace(/\s/g, ''),
+        };
+      });
+      const body = this.excelData.filter((ele) => ele !== this.excelData[0]);
+
+      from(body).pipe(
+        map((row) => {
+          const rowData = {};
+          headers.forEach((header, index) => rowData[header.name] = row[index]);
+          return rowData;
+        })
+      ).subscribe((val) => this.jsonData.push(val));
+    }
   }
 
 }
