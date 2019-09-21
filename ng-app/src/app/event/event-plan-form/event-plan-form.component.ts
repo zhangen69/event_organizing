ae75d04d129d1abf8b55f83af49149aa487d5770
Jacob <zhangen69@gmail.com>
2019-09-21 08:47:41 +0800

    Update event-plan-form.component.ts

--- ng-app/src/app/event/event-plan-form/event-plan-form.component.ts
+++ ng-app/src/app/event/event-plan-form/event-plan-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IStandardFormField } from 'src/app/standard/standard-form-field.interface';
 
 @Component({
   selector: 'app-event-plan-form',
@@ -6,14 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-plan-form.component.css']
 })
 export class EventPlanFormComponent implements OnInit {
-  fields = [
+  fields: IStandardFormField[] = [
     { name: 'name', type: 'string', displayName: 'Name', required: true },
+    {
+      name: 'services', type: 'table', fields: [
+        { name: 'providerService', type: 'ref' },
+        { name: 'quantity', type: 'number' },
+        { name: 'remarks', type: 'textarea' },
+      ]
+    },
+    {
+      name: 'facilities', type: 'table', fields: [
+        { name: 'providerFacility', type: 'ref' },
+        { name: 'quantity', type: 'number' },
+        { name: 'remarks', type: 'textarea' },
+      ]
+    },
+    {
+      name: 'stockItems', type: 'table', fields: [
+        { name: 'stockItem', type: 'ref' },
+        { name: 'quantity', type: 'number' },
+        { name: 'remarks', type: 'textarea' },
+      ]
+    },
     { name: 'totalBudgetAmount', type: 'number', displayName: 'Total Budget (RM)', required: true },
     { name: 'remarks', type: 'textarea', displayName: 'Remarks' },
   ];
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
