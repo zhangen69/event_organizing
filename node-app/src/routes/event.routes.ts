248da573aea4624c31b0afec3052b35f007af649
Jacob Tan <zhangen69@live.co.uk>
2019-09-25 13:59:03 +0800

    Update event.routes.ts

--- node-app/src/routes/event.routes.ts
+++ node-app/src/routes/event.routes.ts
@@ -7,4 +7,14 @@ const routes = new StandardRoutes(service, new Controller(service));
 routes.bypass.getItemById = true;
 const router = routes.router(express.Router());
 
+router.post('/event/venue', (req, res, next) => {
+    // check venue is exist or not
+    // if existed then ignore
+    // else then insert new record
+});
+
+router.get('/event/venues', (req, res, next) => {
+
+});
+
 export default router;
