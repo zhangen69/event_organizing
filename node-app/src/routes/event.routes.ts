5934469a8206c745724832919278ff1eacf5deb3
Jacob <zhangen69@gmail.com>
2019-10-01 20:10:15 +0800

    search venue from event

--- node-app/src/routes/event.routes.ts
+++ node-app/src/routes/event.routes.ts
@@ -1,5 +1,8 @@
-import express from 'express';
+import { from } from 'rxjs';
+import { distinct, map } from 'rxjs/operators';
 import Controller from '../standards/controller';
+import eventModel from '../models/event.model';
+import express from 'express';
 import StandardRoutes from '../standards/routes';
 
 const service = 'event';
@@ -7,14 +10,19 @@ const routes = new StandardRoutes(service, new Controller(service));
 routes.bypass.getItemById = true;
 const router = routes.router(express.Router());
 
-router.post('/event/venue', (req, res, next) => {
-    // check venue is exist or not
-    // if existed then ignore
-    // else then insert new record
-});
-
-router.get('/event/venues', (req, res, next) => {
-
+router.get('/event/venues/:keyword', (req, res, next) => {
+    eventModel.find({
+        venue: new RegExp(`${req.params.keyword}`, 'gi'),
+    }).then((docs) => {
+        const results = [];
+        from(docs).pipe(
+            map((doc) => doc['venue']),
+            distinct(),
+        ).subscribe({
+            next: (result) => results.push(result),
+            complete: () => res.status(200).json(results),
+        });
+    });
 });
 
 export default router;
