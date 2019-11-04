d96344581e343dbdf78c691eb70b712d355783b4
Jacob Tan <zhangen69@live.co.uk>
2019-11-04 14:06:09 +0800

    update solution

--- ng-app/src/app/event/register-event/register-event.component.ts
+++ ng-app/src/app/event/register-event/register-event.component.ts
@@ -1,10 +1,9 @@
-import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { StandardService } from 'src/app/standard/standard.service';
 import { ToastrService } from 'ngx-toastr';
 import { HttpClient } from '@angular/common/http';
 import { PageLoaderService } from 'src/app/templates/page-loader/page-loader.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-register-event',
@@ -14,39 +13,32 @@ import { PageLoaderService } from 'src/app/templates/page-loader/page-loader.ser
 export class RegisterEventComponent implements OnInit {
     mode = 'new';
     registrationForm: any;
+    event: any;
     formData: any = {};
     attendeeData: any = {};
     attendee: any;
     qrcodeLink: string;
-    private formService: StandardService;
-    private attendeeService: StandardService;
 
     constructor(
         private route: ActivatedRoute,
         public http: HttpClient,
-        public dialog: MatDialog,
-        public router: Router,
         public toastr: ToastrService,
         private pageLoaderService: PageLoaderService
     ) {
-        this.formService = new StandardService(this.http, this.dialog, this.router, this.toastr);
-        this.attendeeService = new StandardService(this.http, this.dialog, this.router, this.toastr);
     }
 
     ngOnInit() {
-        this.attendeeService.init('attendee');
-        this.formService.init('registration-form');
         this.route.params.subscribe((params: Params) => {
-            this.formService.fetch(params['formId']).subscribe((res: any) => {
-                this.registrationForm = res.data;
+            this.http.get(environment.apiUrl + '/service/event-plan/getRegistrationForm/' + params['formId']).subscribe((res: any) => {
+                this.registrationForm = res.form;
+                this.event = res.event;
             });
         });
     }
 
     onSubmit() {
         const attendeeData = {
-            registrationForm: this.registrationForm._id,
-            event: this.registrationForm.event,
+            event: this.event._id,
             formData: this.formData
         };
 
@@ -55,7 +47,7 @@ export class RegisterEventComponent implements OnInit {
         });
 
         this.pageLoaderService.toggle(true);
-        this.attendeeService.submit(attendeeData, this.attendeeService.apiUrl + '/register').subscribe((res: any) => {
+        this.http.post(environment.apiUrl + '/service/event-plan/attendee-register', attendeeData).subscribe((res: any) => {
             this.pageLoaderService.toggle(false);
             this.toastr.success(res.message);
             this.mode = 'submitted';
--- node-app/src/routes/event-plan.routes.ts
+++ node-app/src/routes/event-plan.routes.ts
@@ -5,7 +5,6 @@ import eventPlanModel from '../models/event-plan.model';
 import { checkAuth } from '../middlewares/checkAuth';
 import { from } from 'rxjs';
 import { map, distinct } from 'rxjs/operators';
-import nodeMailer from 'nodemailer';
 import appConfigs from '../configs/app.configs';
 import emailQueueModel from '../models/email-queue.model';
 
@@ -14,65 +13,63 @@ const routes = new StandardRoutes(service, new Controller(service));
 const router = routes.router(express.Router());
 
 router.get('/event-plan/venue/list', checkAuth, (req, res, next) => {
-    eventPlanModel.find({ venue: { $exists: true, $ne: null } }).then((docs) => {
-        const results = [];
-        from(docs).pipe(
-            map((doc) => doc['venue']),
-            distinct(),
-        ).subscribe({
-            next: (result) => results.push(result),
-            complete: () => res.status(200).json({ data: results }),
-        });
-    });
+  eventPlanModel.find({ venue: { $exists: true, $ne: null } }).then((docs) => {
+    const results = [];
+    from(docs)
+      .pipe(
+        map((doc) => doc['venue']),
+        distinct(),
+      )
+      .subscribe({
+        next: (result) => results.push(result),
+        complete: () => res.status(200).json({ data: results })
+      });
+  });
 });
 
 router.post(`/${service}/sendRegistrationFormLink`, checkAuth, (req, res, next) => {
-    // console.log('sending email...');
-
-    // const transporter = nodeMailer.createTransport(appConfigs.mail);
-    const mailTemplate = `
+  const mailTemplate = `
             <h1>Registration Form</h1>
             <p>Please click <a href="http://localhost:4200/register/${req.body.formId}">Here</a> to register your seat.</p>
             <p>Check it out.</p>
         `;
 
-    // const mailOptions = {
-    //     from: `EvtOrgMs <${appConfigs.mailAuth.user}>`,
-    //     to: req.body.email,
-    //     subject: `Event Registration Form`,
-    //     html: mailTemplate,
-    // };
+  const emailQueue = new emailQueueModel({
+    to: req.body.email,
+    from: `EvtOrgMs <${appConfigs.mailAuth.user}>`,
+    subject: `Event Registration Form`,
+    message: mailTemplate,
+  });
 
-    const emailQueue = new emailQueueModel({
-        to: req.body.email,
-        from: `EvtOrgMs <${appConfigs.mailAuth.user}>`,
-        subject: `Event Registration Form`,
-        message: mailTemplate,
-    });
-
-    emailQueue.save().then(() => {
-        res.status(200).json({ succeeded: true, message: 'Email sent.' });
-    });
-
-    // transporter.sendMail(mailOptions, (error, info) => {
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    //     console.log('Message %s sent: %s', info.messageId, info.response);
-    //     res.status(200).json({ succeeded: true, message: 'Email sent.' });
-    // });
+  emailQueue.save().then(() => {
+    res.status(200).json({ succeeded: true, message: 'Email sent.' });
+  });
 });
 
 router.get(`/${service}/getRegistrationForm/:id`, (req, res, next) => {
-    eventPlanModel.findById(req.params.id).then((doc) => {
-        const form = doc['registrationForm'];
+  eventPlanModel.findById(req.params.id).then((doc) => {
+    const event = {
+        _id: doc['_id'],
+        name: doc['name'],
+        venue: doc['venue'],
+        dateFrom: doc['dateFrom'],
+        timeFrom: doc['timeFrom'],
+        dateTo: doc['dateTo'],
+        timeTo: doc['timeTo'],
+    };
+    const form = doc['registrationForm'];
 
-        if (form) {
-            res.status(200).json({ form });
-        } else {
-            res.status(500).json({ message: 'form not found' });
-        }
-    });
+    if (form) {
+      res.status(200).json({ form, event });
+    } else {
+      res.status(500).json({ message: 'form not found' });
+    }
+  });
+});
+
+router.post(`/${service}/attendee-register`, (req, res, next) => {
+    console.log(req.body);
+    res.status(200).json({ message: 'ok' });
 });
 
 export default router;
