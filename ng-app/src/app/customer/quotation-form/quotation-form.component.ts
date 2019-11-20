fd521363ac7def80053acae30e8f7f380a1e5f89
Jacob <zhangen69@gmail.com>
2019-11-20 19:37:41 +0800

    quotation

--- ng-app/src/app/customer/quotation-form/quotation-form.component.ts
+++ ng-app/src/app/customer/quotation-form/quotation-form.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+enum QuotationStatus {
+  Open,
+  Confirmed,
+  Revised,
+  Paid,
+  Closed,
+  Cancelled
+}
 
 @Component({
   selector: 'app-quotation-form',
@@ -6,10 +18,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotation-form.component.css']
 })
 export class QuotationFormComponent implements OnInit {
+  formData: any = {};
+  includes = ['customer', 'eventPlan'];
+  fields = [
+    // { name: 'code', type: 'string', required: true },
+    { name: 'customer', type: 'ref', required: true },
+    { name: 'eventPlan', type: 'ref' },
+    { name: 'status', type: 'enum', enum: QuotationStatus, default: QuotationStatus[QuotationStatus.Open], required: true },
+    { name: 'remarks', type: 'textarea' },
+    {
+      name: 'lines',
+      type: 'table',
+      displayName: 'Invoice Items',
+      fields: [
+        { name: 'name', type: 'string', required: true },
+        { name: 'unit', type: 'string', required: true },
+        { name: 'unitPrice', type: 'number', required: true },
+        { name: 'quantity', type: 'number', required: true }
+      ]
+    }
+  ];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      if (params['eventPlan']) {
+        const getEventPlan$ = this.http.get<{ data }>(environment.apiUrl + '/service/event-plan/' + params['eventPlan']).subscribe({
+          next: ({ data }) => {
+            this.formData.eventPlan = data;
+          },
+          complete: () => {
+            getEventPlan$.unsubscribe();
+          }
+        });
+      }
+    });
   }
-
 }
--- ng-app/src/app/customer/quotation-list/quotation-list.component.ts
+++ ng-app/src/app/customer/quotation-list/quotation-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IStandardColumn } from 'src/app/standard/standard.interface';
 
 @Component({
   selector: 'app-quotation-list',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotation-list.component.css']
 })
 export class QuotationListComponent implements OnInit {
+  includes: string[] = ['customer', 'eventPlan'];
+  columns: IStandardColumn[] = [
+     { name: 'customer.name', displayName: 'Customer', width: '250px' },
+     { name: 'eventPlan.name', displayName: 'Event Plan', width: '200px' },
+     { name: 'status', width: '100px' },
+     { name: 'remarks' },
+     { name: 'audit.updatedDate', displayName: 'Updated', type: 'date' },
+  ];
+  filterList = [
+     { type: 'name', display: 'Name', queryType: 'string' },
+  ];
 
   constructor() { }
 
