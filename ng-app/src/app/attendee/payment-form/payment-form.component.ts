c079dbc81e71eaa1c8bc11b11b2fbc4822d0fdb0
Jacob <zhangen69@gmail.com>
2019-11-22 18:05:02 +0800

    update

--- ng-app/src/app/attendee/payment-form/payment-form.component.ts
+++ ng-app/src/app/attendee/payment-form/payment-form.component.ts
@@ -30,26 +30,27 @@ export class PaymentFormComponent implements OnInit {
   fields: IStandardFormField[] = [
     { name: 'provider', type: 'ref', required: true },
     { name: 'eventPlan', type: 'ref', required: true },
+    { name: 'supplierInvoice', type: 'ref', refName: 'code', required: true },
     { name: 'status', type: 'enum', enum: PaymentStatus, default: PaymentStatus[PaymentStatus.Open] },
+    { name: 'amount', type: 'number', required: true },
     { name: 'paymentType', type: 'enum', enum: PaymentType, default: PaymentType[PaymentType.Cash] },
     { name: 'chequeInfo', type: 'object', isShow: item => item.paymentType === PaymentType[PaymentType.Cheque], fields: [
-      { name: 'referenceNumber', type: 'string' },
-      { name: 'payeeName', type: 'string' },
-      { name: 'payeeIdentityNumber', type: 'string' },
-      { name: 'draweeName', type: 'string' },
-      { name: 'draweeIdentityNumber', type: 'string' },
-      { name: 'issuedDate', type: 'date' },
+      { name: 'referenceNumber', type: 'string', required: true },
+      { name: 'payeeName', type: 'string', required: true },
+      { name: 'payeeIdentityNumber', type: 'string', required: true },
+      { name: 'draweeName', type: 'string', required: true },
+      { name: 'draweeIdentityNumber', type: 'string', required: true },
+      { name: 'issuedDate', type: 'date', required: true },
     ]},
     { name: 'bankTransferInfo', type: 'object', isShow: item => item.paymentType === PaymentType[PaymentType.BankTransfer], fields: [
-      { name: 'referenceNumber', type: 'string' },
-      { name: 'bank', type: 'string' },
-      { name: 'accountNumber', type: 'string' },
-      { name: 'payeeName', type: 'string' },
-      { name: 'payeeIdentityNumber', type: 'string' },
-      { name: 'transferedDate', type: 'date' },
+      { name: 'referenceNumber', type: 'string', required: true },
+      { name: 'bank', type: 'string', required: true },
+      { name: 'accountNumber', type: 'string', required: true },
+      { name: 'payeeName', type: 'string', required: true },
+      { name: 'payeeIdentityNumber', type: 'string', required: true },
+      { name: 'transferedDate', type: 'date', required: true },
     ]},
-    { name: 'amount', type: 'number' },
-    { name: 'remarks', type: 'textarea' }
+    { name: 'remarks', type: 'textarea' },
   ];
 
   constructor(private dialog: MatDialog, private route: ActivatedRoute, private http: HttpClient) {}
