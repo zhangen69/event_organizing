040a79f0d13f74e8face35626a1241f97100f436
Jacob Tan <zhangen69@live.co.uk>
2019-11-24 16:58:37 +0800

    stock transaction

--- ng-app/src/app/inventory/stock-transaction-form/stock-transaction-form.component.ts
+++ ng-app/src/app/inventory/stock-transaction-form/stock-transaction-form.component.ts
@@ -1,4 +1,15 @@
+import { ToastrService } from 'ngx-toastr';
+import { environment } from './../../../environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IStandardFormField } from 'src/app/standard/standard.interface';
+import { Router } from '@angular/router';
+
+enum TransactionType {
+  StockIn,
+  StockOut,
+  Adjustment
+}
 
 @Component({
   selector: 'app-stock-transaction-form',
@@ -6,10 +17,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-transaction-form.component.css']
 })
 export class StockTransactionFormComponent implements OnInit {
+  formData: any = {};
+  includes: string[] = ['provider', 'customer', 'eventPlan', 'supplierInvoice', 'invoice'];
+  fields: IStandardFormField[] = [
+    { name: 'type', type: 'enum', enum: TransactionType, default: TransactionType[TransactionType.StockIn], required: true },
+    { name: 'store', type: 'ref', required: true },
+    { name: 'stockItem', type: 'ref', refIncludes: ['category'], required: true },
+    { name: 'quantity', type: 'number', required: true },
+    { name: 'receipt', type: 'ref', refName: 'code' },
+    { name: 'eventPlan', type: 'ref' },
+    { name: 'remarks', type: 'textarea' }
+  ];
 
-  constructor() { }
+  constructor(private toastr: ToastrService, private http: HttpClient, private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit(formData) {
+    const newStockTransaction = Object.assign(
+      {
+        stockItemName: formData.stockItem.name,
+        stockItemUnit: formData.stockItem.unit,
+        stockItemUnitPrice: formData.stockItem.unitPrice,
+        stockItemCategory: formData.stockItem.category
+      },
+      formData
+    );
+    const req$ = this.http.post(environment.apiUrl + '/service/stock-transaction', newStockTransaction).subscribe({
+      next: () => {
+        this.toastr.success('Stock Transaction is created');
+        if (window.history.length > 0) {
+          window.history.back()
+        } else {
+          this.router.navigate(['/stock-transaction/list']);
+        }
+      },
+      complete: () => req$.unsubscribe()
+    });
   }
-
 }
