6dc8b3e420b5af39cb4276cff457545838dfc99c
Jacob Tan <zhangen69@live.co.uk>
2019-11-28 23:05:52 +0800

    stock transaction routeing

--- node-app/src/routes/stock-item.routes.ts
+++ node-app/src/routes/stock-item.routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import Controller from '../standards/controller';
 import StandardRoutes from '../standards/routes';
+import { checkAuth } from '../middlewares/checkAuth';
 
 const service = 'stock-item';
 const routes = new StandardRoutes(service, new Controller(service));
 const router = routes.router(express.Router());
+
+router.get('/stock-item/mapStockTransaction', checkAuth, (req, res, next) => {
+  const queryModel = req.query.queryModel || '{}';
+  this.modelService.fetchAll(JSON.parse(queryModel)).then((result) => {
+    const stockTransactionIds = result.data.map(item => item._id);
+  });
+
+});
 
 export default router;
--- node-app/src/routes/stock-transaction.routes.ts
+++ node-app/src/routes/stock-transaction.routes.ts
@@ -1,9 +1,29 @@
 import express from 'express';
 import Controller from '../standards/controller';
 import StandardRoutes from '../standards/routes';
+import { checkAuth } from '../middlewares/checkAuth';
+import stockItemModel from '../models/stock-item.model';
+import stockTransactionModel from '../models/stock-transaction.model';
 
 const service = 'stock-transaction';
 const routes = new StandardRoutes(service, new Controller(service));
 const router = routes.router(express.Router());
+
+router.get('/stock-transaction/getQuantityInStock/:stockItemId', checkAuth, (req, res, next) => {
+  stockItemModel.findById(req.params.stockItemId).then(doc => {
+    stockTransactionModel.find({ stockItem: doc._id }).then((docs: any[]) => {
+      let quantity = 0;
+      const totalStockIn = docs.filter(item => item.type === 'StockIn').reduce((acc, item) => acc + item.quantity, 0);
+      const totalStockOut = docs.filter(item => item.type === 'StockOut').reduce((acc, item) => acc + item.quantity, 0);
+
+      quantity = totalStockIn - totalStockOut;
+
+      res.json({
+        stockItem: doc,
+        quantity,
+      });
+    });
+  });
+});
 
 export default router;
