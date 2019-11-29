ad181637804b6ef8b96ffc482c0994045c4b480d
Jacob <zhangen69@gmail.com>
2019-11-29 16:53:01 +0800

    title

--- ng-app/src/app/user/user-change-password/user-change-password.component.ts
+++ ng-app/src/app/user/user-change-password/user-change-password.component.ts
@@ -2,6 +2,8 @@ import { PageLoaderService } from 'src/app/templates/page-loader/page-loader.ser
 import { UserService } from './../../services/user.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user-change-password',
@@ -14,10 +16,11 @@ export class UserChangePasswordComponent implements OnInit {
     newPassword: [null, Validators.required],
   });
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService, private pageLoaderService: PageLoaderService) {
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private pageLoaderService: PageLoaderService, private titleService: Title) {
     this.pageLoaderService.toggle(true);
     this.userService.init('user');
     this.pageLoaderService.toggle(false);
+    this.titleService.setTitle('Change Password - ' + environment.title);
   }
 
   ngOnInit() {
--- ng-app/src/app/user/user-profile/user-profile.component.ts
+++ ng-app/src/app/user/user-profile/user-profile.component.ts
@@ -1,3 +1,5 @@
+import { environment } from './../../../environments/environment';
+import { Title } from '@angular/platform-browser';
 import { PageLoaderService } from 'src/app/templates/page-loader/page-loader.service';
 import { UserService } from './../../services/user.service';
 import { ToastrService } from 'ngx-toastr';
@@ -21,7 +23,7 @@ export class UserProfileComponent implements OnInit {
   });
   imagesPreview: any = {};
 
-  constructor(private formBuilder: FormBuilder, private toastr: ToastrService, private userService: UserService, private pageLoaderService: PageLoaderService) { }
+  constructor(private formBuilder: FormBuilder, private toastr: ToastrService, private userService: UserService, private pageLoaderService: PageLoaderService, private titleService: Title) { }
 
   ngOnInit() {
     this.pageLoaderService.toggle(true);
@@ -29,6 +31,7 @@ export class UserProfileComponent implements OnInit {
       this.formData.patchValue(res.data);
       this.pageLoaderService.toggle(false);
     });
+    this.titleService.setTitle('User Profile - ' + environment.title);
   }
 
   onImagePicked(event: Event) {
