478992f74f8bdcedcc84ee5ee01d2d77e1f6b09a
Jacob Tan <zhangen69@live.co.uk>
2019-12-02 02:04:03 +0800

    invoice list

--- ng-app/src/app/customer/invoice-list/invoice-list.component.ts
+++ ng-app/src/app/customer/invoice-list/invoice-list.component.ts
@@ -9,15 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class InvoiceListComponent implements OnInit {
   includes: string[] = ['customer', 'eventPlan'];
   columns: IStandardColumn[] = [
-    { name: 'code', width: '150px' },
-     { name: 'customer.name', displayName: 'Customer', width: '250px', format: 'link', link: (item) => '/customer/view/' + item.customer._id },
-     { name: 'eventPlan.name', displayName: 'Event Plan', width: '200px' },
-     { name: 'status', width: '100px' },
+    { name: 'code', width: '80px' },
+     { name: 'customer.name', displayName: 'Customer', width: '150px', format: 'link', link: (item) => '/customer/view/' + item.customer._id },
+     { name: 'eventPlan.name', displayName: 'Event Plan', width: '150px' },
+     { name: 'status', width: '80px' },
      { name: 'remarks' },
      { name: 'audit.updatedDate', displayName: 'Updated', type: 'date' },
   ];
   filterList = [
-     { type: 'name', display: 'Name', queryType: 'string' },
+     { type: 'code', displayName: 'Code', queryType: 'string' },
+     { type: 'status', displayName: 'Status', queryType: 'string' },
   ];
 
   constructor() { }
