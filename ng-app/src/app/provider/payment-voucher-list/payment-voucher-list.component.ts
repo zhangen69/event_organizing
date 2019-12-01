9c15b4a0380722523a755bec42904bdecbfff66f
Jacob Tan <zhangen69@live.co.uk>
2019-12-02 02:06:56 +0800

    Update payment-voucher-list.component.ts

--- ng-app/src/app/provider/payment-voucher-list/payment-voucher-list.component.ts
+++ ng-app/src/app/provider/payment-voucher-list/payment-voucher-list.component.ts
@@ -9,13 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class PaymentVoucherListComponent implements OnInit {
   includes = ['provider', 'receipt', 'eventPlan'];
   columns: IStandardColumn[] = [
-    { name: 'code' },
-    { name: 'provider.name', displayName: 'Provider' },
-    { name: 'eventPlan.name', displayName: 'Event Plan' },
-    { name: 'status' },
+    { name: 'code', width: '80px' },
+    { name: 'provider.name', displayName: 'Provider', format: 'link', link: item => item.provider ? '/provider/view/' + item.provider._id : '' },
+    { name: 'eventPlan.name', displayName: 'Event Plan', format: 'link', link: item => item.eventPlan ? '/event-plan/view/' + item.eventPlan._id : '' },
+    { name: 'status', width: '80px' },
     { name: 'audit.updatedDate', displayName: 'Updated', type: 'date' }
   ];
-  filterList = [{ type: 'name', display: 'Name', queryType: 'string' }];
+  filterList = [
+    { type: 'code', displayName: 'Code', queryType: 'string' },
+    { type: 'status', displayName: 'Status', queryType: 'string' },
+  ];
 
   constructor() {}
 
