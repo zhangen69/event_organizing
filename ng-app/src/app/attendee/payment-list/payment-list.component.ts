0a80ae3dff021c3e881e7f79d0edb13c1d992fc2
Jacob Tan <zhangen69@live.co.uk>
2019-12-02 02:16:45 +0800

    Update payment-list.component.ts

--- ng-app/src/app/attendee/payment-list/payment-list.component.ts
+++ ng-app/src/app/attendee/payment-list/payment-list.component.ts
@@ -10,8 +10,15 @@ export class PaymentListComponent implements OnInit {
   includes: string[] = ['provider', 'customer', 'eventPlan', 'supplierInvoice', 'invoice'];
   columns: IStandardColumn[] = [
     { name: 'code', width: '80px' },
-    { name: 'provider.name', displayName: 'Provider' },
-    { name: 'customer.name', displayName: 'Customer' },
+    { name: 'to', displayName: 'Provicer/Customer', width: '200px', format: 'template', template: item => {
+      if (item.type === 'Customer' && item.invoice) {
+        return item.customer.name;
+      } else if (item.type === 'Provider' && item.supplierInvoice) {
+        return item.provider.name;
+      }
+    } },
+    // { name: 'provider.name', displayName: 'Provider' },
+    // { name: 'customer.name', displayName: 'Customer' },
     {
       name: 'invoice.code',
       displayName: 'Pay For',
@@ -25,14 +32,15 @@ export class PaymentListComponent implements OnInit {
         }
       }
     },
-    { name: 'eventPlan.name', displayName: 'Event Plan', format: 'link', link: item => '/event-plan/view/' + item.eventPlan._id },
-    { name: 'amount', type: 'currency' },
-    { name: 'paymentType' },
-    { name: 'status' }
+    { name: 'eventPlan.name', displayName: 'Event Plan', width: '150px', format: 'link', link: item => '/event-plan/view/' + item.eventPlan._id },
+    { name: 'amount', type: 'currency', width: '80px' },
+    { name: 'paymentType', width: '80px' },
+    { name: 'status', width: '80px' }
   ];
   filterList = [
     { type: 'code', displayName: 'Code', queryType: 'string' },
-    { type: 'paymentType', displayName: 'Payment Type', queryType: 'string' }
+    { type: 'paymentType', displayName: 'Payment Type', queryType: 'string' },
+    { type: 'status', displayName: 'Status', queryType: 'string' },
   ];
 
   constructor() {}
