244426d7f571fbe897c219068d09bb90ea3d2b3c
Jacob Tan <zhangen69@live.co.uk>
2019-12-02 02:04:15 +0800

    quotation list

--- ng-app/src/app/customer/quotation-list/quotation-list.component.ts
+++ ng-app/src/app/customer/quotation-list/quotation-list.component.ts
@@ -9,14 +9,17 @@ import { IStandardColumn } from 'src/app/standard/standard.interface';
 export class QuotationListComponent implements OnInit {
   includes: string[] = ['customer', 'eventPlan'];
   columns: IStandardColumn[] = [
-    { name: 'code', width: '150px' },
-    { name: 'customer.name', displayName: 'Customer', width: '250px', format: 'link', link: item => '/customer/view/' + item.customer._id },
-    { name: 'eventPlan.name', displayName: 'Event Plan', width: '200px', format: 'link', link: item => '/event-plan/view/' + item.eventPlan._id },
-    { name: 'status', width: '100px' },
+    { name: 'code', width: '80px' },
+    { name: 'customer.name', displayName: 'Customer', width: '150px', format: 'link', link: item => '/customer/view/' + item.customer._id },
+    { name: 'eventPlan.name', displayName: 'Event Plan', width: '150px', format: 'link', link: item => '/event-plan/view/' + item.eventPlan._id },
+    { name: 'status', width: '80px' },
     { name: 'remarks' },
     { name: 'audit.updatedDate', displayName: 'Updated', type: 'date' }
   ];
-  filterList = [{ type: 'name', display: 'Name', queryType: 'string' }];
+  filterList = [
+    { type: 'code', displayName: 'Code', queryType: 'string' },
+    { type: 'status', displayName: 'Status', queryType: 'string' },
+  ];
 
   constructor() {}
 
