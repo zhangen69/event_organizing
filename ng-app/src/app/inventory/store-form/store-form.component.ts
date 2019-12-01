47fde285f720fe179233bef4672ce7bfa87b8855
Jacob Tan <zhangen69@live.co.uk>
2019-12-02 02:19:26 +0800

    Update store-form.component.ts

--- ng-app/src/app/inventory/store-form/store-form.component.ts
+++ ng-app/src/app/inventory/store-form/store-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+enum StoreStatus {
+  Available,
+  Unavailable,
+}
+
 @Component({
   selector: 'app-store-form',
   templateUrl: './store-form.component.html',
@@ -8,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class StoreFormComponent implements OnInit {
   fields = [
      { name: 'name', type: 'string', required: true },
+     { name: 'status', type: 'enum', enum: StoreStatus, default: StoreStatus[StoreStatus.Available] },
      { name: 'description', type: 'string' },
   ];
 
